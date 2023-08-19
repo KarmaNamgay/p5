23bf1410d13da8dd2c236b3b872ae682e355454b
KarmaNamgay <132356064+KarmaNamgay@users.noreply.github.com>
2023-08-19 09:28:34 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(640,480);
+    canvas.position(110,250);
+    video=createCapture(VIDEO);
+    video.hide();
+    color="";
+    
+}
+function draw(){
+    image(video,0,0,640,480);
+    tint(color);
+}
+
+function addFilter(){
+    color=document.getElementById('color').value;
+}
+
+function take_snapshot(){
+    save("image.png");
+}
